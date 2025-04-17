7c0d843c9dfcf5d62318b36cef2030d146d4002a
Moritz Kraus <moritz.kraus@makandra.de>
2025-04-17 14:30:40 +0200

    chore: fix linting issues

--- manifests/rubygems.pp
+++ manifests/rubygems.pp
@@ -4,7 +4,6 @@ define rbenv::rubygems (
   $home,
   $ruby,
 ) {
-
   exec { "update_rubygems_${user}_${version}":
     command     => "${home}/.rbenv/shims/gem update --system ${version}",
     unless      => "${home}/.rbenv/shims/gem -v | /bin/grep ${version}",
@@ -13,5 +12,4 @@ define rbenv::rubygems (
     environment => "HOME=${home}",
     require     => Exec["rbenv::compile ${user} ${ruby}"],
   }
-
 }
